571eb9fa695d73f466248a2d75e225c2dbade665
jred2018 <43849071+jred2018@users.noreply.github.com>
2018-11-27 18:23:35 +0000

    Update CardboardBox.java

--- ADPROCCW/src/CardboardBox/CardboardBox.java
+++ ADPROCCW/src/CardboardBox/CardboardBox.java
@@ -1,14 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package CardboardBox;
 
-/**
- *
- * @author tama3
- */
-public abstract class CardboardBox {
+/*
+* What I have currently, is this class in addition to an order class?
+*/
+package cardboardbox;
+
+
+public class CardboardBox {
+
+    double width, length, height, cost;
+    int quantity, grade;
+    boolean sealableTop;
+    
+    public CardboardBox(double bwidth, double blength, double bheight, int bquantity, int bgrade, boolean hassealableTop){
+        width = bwidth;
+        length = blength;
+        height = bheight;
+        quantity = bquantity;
+        grade = bgrade;
+        sealableTop = hassealableTop;
+    }
+    
+    public CardboardBox(double bwidth, double blength, double bheight, int bquantity, int bgrade, boolean hassealableTop, int colour){
+        width = bwidth;
+        length = blength;
+        height = bheight;
+        quantity = bquantity;
+        grade = bgrade;
+        sealableTop = hassealableTop;
+    }
+    
+    public CardboardBox(double bwidth, double blength, double bheight, int bquantity, int bgrade, boolean hassealableTop, int colour, boolean reinforcedBottom, boolean reinforcedCorner){
+        width = bwidth;
+        length = blength;
+        height = bheight;
+        quantity = bquantity;
+        grade = bgrade;
+        sealableTop = hassealableTop;
+    }
+    
+    
+    public double area(){
+        double area;
+        area = width * length * height;
+        return area;
+    }
+    
+    
+    public double pricePoint(){
+        switch(grade){
+            case 1: return 0.55;
+            case 2: return 0.65;
+            case 3: return 0.82;
+            case 4: return 0.98;
+            case 5: return 1.50;
+            default: return 0.0;
+        }
+    }
+    
+    public double basicCost(){
+        double price;
+        price = (area() * pricePoint());
+        return price;
+    }
+        
+        
+    
     
 }
