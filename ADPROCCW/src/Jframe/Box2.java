2f3cd95cd3426ed337477a0655150e527733a280
jred2018 <43849071+jred2018@users.noreply.github.com>
2018-12-01 00:22:01 +0000

    Create Box2.java

---
+++ ADPROCCW/src/Jframe/Box2.java
@@ -0,0 +1,14 @@
+package adproccw;
+
+public class Box2 extends Box {
+    
+    public Box2(double Length, double Width, double Height,
+            int grade, boolean sealableTop){
+            
+            super(length, width, height, grade, 1, false, false, sealableTop);
+            priceModifier = 0.12;
+}
+    
+            
+    
+}
