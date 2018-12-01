7352c2c1a114fd07445ecefc44ceee40e57e2dc1
jred2018 <43849071+jred2018@users.noreply.github.com>
2018-12-01 00:23:27 +0000

    Create Box4.java

---
+++ ADPROCCW/src/Jframe/Box4.java
@@ -0,0 +1,14 @@
+package adproccw;
+
+public class Box4 extends Box {
+    
+    public Box4(double Length, double Width, double Height,
+            int grade, boolean sealableTop){
+            
+            super(length, width, height, grade, 2, false, true, sealableTop);
+            priceModifier = 0.28;
+}
+    
+            
+    
+}
